ba0876aec17a749e83ee4c921449bb2de4d7a62f
Cristiann <cristiannsilva2015@gmail.com>
2023-02-02 21:05:53 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+    github: 'Cristyann',
+    youtube: 'rocketseat',
+    twitch: 'cristyann_silva',
+    instagram: 'cristyann_silva',
+    twitter: 'cristyann.dev'
+  }
+  
+  function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+      const social = li.getAttribute('class')
+  
+      li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    }
+  }
+  
+  changeSocialMediaLinks()
+  
+  function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        UserImage.src = data.avatar_url
+        userLogin.textContent = data.login
+      })
+  }
+  
+  getGitHubProfileInfos()
